055a9cd9c325e15821470d4a09ce55f502f44b7b
Harmeet Singh <virdiharmet@08gmail.com>
2025-12-22 15:35:13 +0530

    Solve the error of nav bar

--- src/pages/FitnessScore.jsx
+++ src/pages/FitnessScore.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from "react";
-import Navbar from "../components/ui/NavBar";
+import Navbar from "./NavBar";
 import { Button } from "../components/ui/button";
 
 export default function FitnessScore({ user, setUser }) {
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -24,7 +24,7 @@ import {
   Moon,
   Sun
 } from "lucide-react";
-import { Avatar, AvatarImage, AvatarFallback } from "./avatar";
+import { Avatar, AvatarImage, AvatarFallback } from "../components/ui/avatar";
 
 export default function ProfessionalNavbar({ user, setUser, theme, setTheme }) {
   const navigate = useNavigate();
